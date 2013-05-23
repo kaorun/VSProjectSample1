666ef73734e86b19cea4d0fd40fdd6b12c4f6acc
Kaoru Nakajima <kaorun@labs.cybozu.co.jp>
2013-05-23 18:36:09 +0900

    VS generated templates.

---
+++ WPAppSample/WPAppSample/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WPAppSample.Resources;
+
+namespace WPAppSample
+{
+    /// <summary>
+    /// 文字列リソースにアクセスできるようにします。
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ WPAppSample/WPAppSample/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using WPAppSample.Resources;
+
+namespace WPAppSample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // コンストラクター
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // ListBox コントロールのデータ コンテキストをサンプル データに設定します
+            DataContext = App.ViewModel;
+
+            // ApplicationBar をローカライズするためのサンプル コード
+            //BuildLocalizedApplicationBar();
+        }
+
+        // ViewModel Items のデータを読み込みます
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+        // ローカライズされた ApplicationBar を作成するためのサンプル コード
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // ページの ApplicationBar を ApplicationBar の新しいインスタンスに設定します。
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // 新しいボタンを作成し、テキスト値を AppResources のローカライズされた文字列に設定します。
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // AppResources のローカライズされた文字列で、新しいメニュー項目を作成します。
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
